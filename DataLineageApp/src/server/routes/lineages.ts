3465eb68cc2084010c1cadeed1c5a41f41ce7c4b
Frank <Frank@Home>
2018-05-08 23:44:54 +0800

    add lineages api

---
+++ DataLineageApp/src/server/routes/lineages.ts
@@ -0,0 +1,12 @@
+﻿import * as express from "express";
+
+const router = express.Router();
+
+/* GET home page. */
+router.get("/:address", (req, res) => {
+    res.json({
+        address: req.param("address")
+    });
+});
+
+export default router;
--- DataLineageApp/src/server/server-app.ts
+++ DataLineageApp/src/server/server-app.ts
@@ -4,9 +4,9 @@ import path = require("path");
 import logger = require("morgan");
 import cookieParser = require("cookie-parser");
 import bodyParser = require("body-parser");
+import lineages from "./routes/lineages";
 
 var routes = require("./routes/index");
-var users = require("./routes/users");
 
 var app = express();
 
@@ -23,10 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
-app.use("/users", users);
+app.use("/api/lineages", lineages);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     var err = new Error("Not Found");
     (err as any).status = 404;
     next(err);
@@ -37,27 +37,29 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-    app.use(function (err, req, res, next) {
+    app.use(<any>((err, req, res, next) => {
         res.status(err.status || 500);
-        res.render("error", {
-            message: err.message,
-            error: err
-        });
-    });
+        res.render("error",
+            {
+                message: err.message,
+                error: err
+            });
+    }));
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(<any>((err, req, res, next) => {
     res.status(err.status || 500);
-    res.render("error", {
-        message: err.message,
-        error: {}
-    });
-});
+    res.render("error",
+        {
+            message: err.message,
+            error: {}
+        });
+}));
 
 app.set("port", process.env.PORT || 3000);
 
-var server = app.listen(app.get("port"), function () {
+const server = app.listen(app.get("port"), () => {
     debug("Express server listening on port " + server.address().port);
 });
