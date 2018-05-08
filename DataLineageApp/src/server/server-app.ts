413744c63847b2df50c3e7dc72464178fbc02281
Frank <Frank.Chen@dnvgl.com>
2018-05-08 17:19:28 +0800

    import types for typescript

--- DataLineageApp/src/server/server-app.ts
+++ DataLineageApp/src/server/server-app.ts
@@ -1,34 +1,33 @@
-﻿var debug = require('debug');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+﻿import debug = require("debug");
+import express = require("express");
+import path = require("path");
+import logger = require("morgan");
+import cookieParser = require("cookie-parser");
+import bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require("./routes/index");
+var users = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use("/", routes);
+app.use("/users", users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error("Not Found");
     (err as any).status = 404;
     next(err);
 });
@@ -37,10 +36,10 @@ app.use(function (req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render("error", {
             message: err.message,
             error: err
         });
@@ -51,14 +50,14 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
         message: err.message,
         error: {}
     });
 });
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+var server = app.listen(app.get("port"), function () {
+    debug("Express server listening on port " + server.address().port);
 });
